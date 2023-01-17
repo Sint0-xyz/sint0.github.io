3f259aded183f949dedc57e29c4917b26f68cc77
Sint0 <101415806+Sint0-xyz@users.noreply.github.com>
2023-01-17 03:54:36 +0000

    Create title2.js

---
+++ Js/title2.js
@@ -0,0 +1,15 @@
+let x = 0;
+const title = []
+const nick = 'Jabber'
+for (let i = 1; i <= nick.length; ++i) {
+title.push(nick.slice(0, i))
+}
+for (let i = 1; i < nick.length; ++i) {
+title.push(nick.slice(0, -i))
+}
+title.push('&lrm;')
+function loop() {
+    document.getElementsByTagName('title')[0].innerHTML = title[x++ % title.length];
+    setTimeout(() => loop(), 1090)
+}
+loop()
